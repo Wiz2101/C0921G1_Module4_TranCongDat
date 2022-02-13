aa5b815e06d18f3d1e23dbc743142152fda29282
Wiz2101 <trancongdat.2101@gmail.com>
2022-02-14 00:09:49 +0700

    update s12

---
+++ s12_jquery_ajax/bai_tap/tich_hop_ajax_cho_blog/src/main/java/com/codegym/controller/BlogController.java
@@ -0,0 +1,83 @@
+package com.codegym.controller;
+
+import com.codegym.model.Blog;
+import com.codegym.service.IBlogService;
+import com.codegym.service.ICategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Optional;
+
+
+@Controller
+public class BlogController {
+    @Autowired
+    IBlogService blogService;
+
+    @Autowired
+    ICategoryService categoryService;
+
+    @GetMapping("/blog")
+    public String showHomepage(Optional<String> keyword, Optional<Long> category, Model model, @PageableDefault(size = 5) Pageable pageable) {
+        Page<Blog> blogList = blogService.findAll(pageable);
+        model.addAttribute("categoryList", categoryService.findAll());
+        if (!keyword.isPresent() || keyword.get().equals("")) {
+            if (!category.isPresent()) {
+                model.addAttribute("blogList", blogService.findAll(pageable));
+            } else {
+                model.addAttribute("blogList", categoryService.searchById(category.get(), pageable));
+                model.addAttribute("category", category.get());
+            }
+        } else if (!category.isPresent()) {
+            model.addAttribute("keyword", keyword.get());
+            model.addAttribute("blogList", blogService.findAllByAuthorContaining(keyword.get(), pageable));
+        } else {
+            model.addAttribute("blogList", blogService.searchByAuthorAndType(keyword.get(), category.get(), pageable));
+        }
+        return "/blog/view";
+    }
+
+    @GetMapping("/blog/create")
+    public String showCreate(Model model) {
+        model.addAttribute("blog", new Blog());
+        model.addAttribute("categoryList", categoryService.findAll());
+        return "/blog/create";
+    }
+
+    @PostMapping("/blog/create")
+    public String create(@ModelAttribute Blog blog, RedirectAttributes redirectAttributes) {
+        blogService.saveOrUpdate(blog);
+        redirectAttributes.addFlashAttribute("createMsg", "Create or Update Successfully!");
+        return "redirect:/blog";
+    }
+
+    @GetMapping("/blog/edit/{id}")
+    public String showEdit(@PathVariable Long id, Model model) {
+        Blog blog = blogService.findById(id);
+        model.addAttribute("blog", blog);
+        return "/blog/edit";
+    }
+
+    @GetMapping("/blog/delete/{id}")
+    public String delete(@PathVariable Long id, RedirectAttributes redirectAttributes) {
+        blogService.delete(id);
+        redirectAttributes.addFlashAttribute("deleteMsg", "Delete Successfully!");
+        return "redirect:/blog";
+    }
+
+//    @GetMapping("blog/search")
+//    public String search(@RequestParam String keyword, @RequestParam Long category, Model model, Pageable pageable){
+//        Page<Blog> blogList = blogService.searchByAuthorAndType(keyword,category,pageable);
+//        model.addAttribute("blogList",blogList);
+//        return "blog/view";
+//    }
+}
---
+++ s12_jquery_ajax/bai_tap/tich_hop_ajax_cho_blog/src/main/java/com/codegym/service/BlogService.java
@@ -0,0 +1,44 @@
+package com.codegym.service;
+
+import com.codegym.model.Blog;
+import com.codegym.repository.IBlogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BlogService implements IBlogService {
+    @Autowired
+    IBlogRepository blogRepository;
+
+    @Override
+    public Page<Blog> findAll(Pageable pageable) {
+        return blogRepository.findAll(pageable);
+    }
+
+    @Override
+    public void saveOrUpdate(Blog blog) {
+        blogRepository.save(blog);
+    }
+
+    @Override
+    public void delete(Long id) {
+        blogRepository.deleteById(id);
+    }
+
+    @Override
+    public Blog findById(Long id) {
+        return blogRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Page<Blog> findAllByAuthorContaining(String keyword, Pageable pageable){
+        return blogRepository.findAllByAuthorContaining(keyword,pageable);
+    }
+
+    @Override
+    public Page<Blog> searchByAuthorAndType(String author, Long categoryId, Pageable pageable) {
+        return blogRepository.searchByAuthorAndType("%"+author+"%",categoryId,pageable);
+    }
+}
